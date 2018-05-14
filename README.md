8b5bdb8b0dd8c7b804a83c3267df3685f1da68fe
Steve Mahlstedt <steve.mahlstedt@gmail.com>
2018-05-14 12:37:15 -0400

    Added enemy and player class and methods

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-frontend-nanodegree-arcade-game
-===============================
+# Arcade Game Project
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+## Table of Contents
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+* [Instructions](#instructions)
+* [Dependencies](#dependencies)
+* [Contributing](#contributing)
+
+### Instructions
+(From project overview)
+In this game you have a Player and Enemies (Bugs). 
+1. The goal of the player is to reach the water, without colliding into any one of the enemies. 
+2. The player can move left, right, up and down. 
+3. The enemies move in varying speeds on the paved block portion of the scene. 
+4. Once a the player collides with an enemy, the game is reset and the player moves back to the start square. 
+5. Once the player reaches the water the game is won.
+
+### Dependencies
+This project requires no dependencies.
+
+### Contributing
+
+This repository is a project requirement for Udacity. Therefore, we most likely will not accept pull requests.
+
+
--- js/app.js
+++ js/app.js
@@ -1,19 +1,18 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(speed = 1, startY = 1, startX = -1) {
     this.sprite = 'images/enemy-bug.png';
+    this.x = startX * 101;
+    this.y = startY * 83;
+    this.speed = speed;
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.x = this.x + (80 * this.speed * dt); //Gives enemies movement
+    if (this.x > (5 * 101) + 101) {
+        this.x = -101;
+    }
+
+    this.checkCollision();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,16 +20,69 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// handles collision with player
+Enemy.prototype.checkCollision = function() {
+    if (player.x >= this.x - 75 && player.x <= (this.x + 85)){
+        if (player.y >= this.y - 40 && player.y <= (this.y + 43)) {
+            player.backToStart();
+        }
+     }
+};
 
+// Player we control
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 2 * 101;
+    this.y = 5 * 83;
+}
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+Player.prototype.update = function() {
+    var that = this;
 
+    if (this.y < 0) { this.y = 0; } //makes sure that the player doesn't move off the screen
+    if (this.y > (5 * 83)) { this.y = (5 * 83); }
+    if (this.x < 0) { this.x = 0; }
+    if (this.x > (4 * 101)) { this.x = (4 * 101); }
+    
+    if (this.y < 83) {  //win condition 
+        setTimeout(function(){ 
+            that.backToStart();
+        },  1000);
+    }
+        
+};
 
+//Draws player on screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Links player movement to arrow keys
+Player.prototype.handleInput = function(key) {
+    if (key === 'up') { this.y = this.y - 83; }
+    if (key === 'right') { this.x = this.x + 101; }
+    if (key === 'down') { this.y = this.y + 83; }
+    if (key === 'left') { this.x = this.x - 101; }  
+};
+
+//Returns player to the beginning
+Player.prototype.backToStart = function() {
+    player.x = 2 * 101;
+    player.y = 5 * 83;
+};
+
+// Instantiating enemies
+var enemyOne = new Enemy;
+var enemyTwo = new Enemy(2, 2);
+var enemyThree = new Enemy(1, 3, 0);
+var enemyFour = new Enemy(1, 3, -3);
+var enemyFive = new Enemy(3, 1, -2);
+
+//Player obj
+var player = new Player;
+
+//Array of enemies that engine uses to draw
+var allEnemies = [enemyOne, enemyTwo, enemyThree, enemyFour, enemyFive];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
